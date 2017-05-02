e2bb11dc386083e8c4438d6d280d5112cd872c2a
Ryan Volz <rvolz@mit.edu>
2017-05-02 15:38:42 -0400

    RF survey scripts: Pass all command line arguments to rf-survey.py.

--- software/survey_fm_narrow.sh
+++ software/survey_fm_narrow.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 
-python $BASEDIR/rf-survey.py -c cha,chb -d "A:RX1 A:RX2" --f-start 87.5e6 --f-end 107.9e6 --f-interval 200e3 -l 60 -r 1e6 -i 4 -b 10e6 -g 0 $1
+python $BASEDIR/rf-survey.py -c cha,chb -d "A:RX1 A:RX2" --f-start 87.5e6 --f-end 107.9e6 --f-interval 200e3 -l 60 -r 1e6 -i 4 -b 10e6 -g 0 "$@"
