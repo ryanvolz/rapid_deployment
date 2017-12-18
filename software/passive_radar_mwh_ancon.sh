f4c674b4aaa22fdba537c0e9f052baf2d8396d89
Ryan Volz <rvolz@mit.edu>
2017-12-18 16:26:44 -0500

    Update passive radar script timing so cycle is 1 hour.

--- software/passive_radar_mwh_ancon.sh
+++ software/passive_radar_mwh_ancon.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 
-python $BASEDIR/rf_hop.py -c cha,chb -d "A:RX1 A:RX2" -f 88.3e6,102.7e6,105.5e6,106.3e6,107.7e6 -l 1150,600,600,600,600 --num_cycles 48 -r 1e6 -i 8 -b 10e6 -g 0 "$@"
+python $BASEDIR/rf_hop.py -c cha,chb -d "A:RX1 A:RX2" -f 88.3e6,102.7e6,105.5e6,106.3e6,107.7e6 -l 1105,600,600,600,600 --num_cycles 48 -r 1e6 -i 8 -b 10e6 -g 0 -s 0 -p 5 "$@"
